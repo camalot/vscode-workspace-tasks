cec0ddeb25ceb5c00da1452ab4d7aeea52167671
Ryan Conrad <camalot@gmail.com>
2026-01-25 09:59:17 -0600

    feat(tasks): some pre-work and samples for supporting jupyter notebooks

--- scripts/svg-convert.ps1
+++ scripts/svg-convert.ps1
@@ -5,7 +5,7 @@ param(
     [string]$pattern = "../res/icons/**/*.svg"
 )
 
-foreach ($file in Get-ChildItem -Recurse -Filter $pattern) {
+foreach ($file in Get-ChildItem -Recurse $pattern) {
   Write-Output "Converting $($file.FullName) to PNG with width $Width"
   magick -density 300 -background none $file.FullName -resize ${Width}x ($file.DirectoryName + "\" + $file.BaseName + ".png")
 }
