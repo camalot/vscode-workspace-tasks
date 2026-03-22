35cdebee115c6f0060236b83df03348f06148634
Ryan Conrad <camalot@gmail.com>
2026-03-22 01:20:30 +0000

    chore: remove test files

--- patch-taskfiles.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/services/taskFilesService.ts', 'utf8');
-
-code = code.replace(/vscode\.workspace\.findFiles\(([^,]+)(?:,\s*[^)]+)?\)/g, "vscode.workspace.findFiles($1, '{**/.vscode-test/**,**/node_modules/**,**/.git/**}')");
-
-fs.writeFileSync('src/services/taskFilesService.ts', code);
